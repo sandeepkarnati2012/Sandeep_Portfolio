e1e7f182bec234f7723384dd55fb663419bd02f6
sandeepkarnati2012 <sandeepkarnati2012@gmail.com>
2025-02-14 17:05:22 -0500

    'changes'

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -16,22 +16,35 @@ export const About=()=>{
                     <li className={styles.aboutItem}>
                         <img src={pic1} alt="Cursor Icon"  />
                         <div className={styles.aboutItemText}>
-                            <h3>Frontend Developer</h3>
-                            <p>I'm a frontend developer with experience in building responsive and optimized sites</p>
+                            <h3>Education</h3>
+                            <h4>Master of Science in Computer Science - GPA: 3.4 </h4>
+                            <p>Wright State University, Dayton, OH (April 2024)</p>
+                            <h4>Bachelor of Science in Information Technology - GPA: 3.3</h4>
+                            <p>Anil Neerukonda Institute of Technology and Sciences, Visakhapatnam, India (May 2022)</p>
                         </div>
                     </li>
                     <li className={styles.aboutItem}>
                         <img src={pic2} alt="server icon"/>
                         <div className={styles.aboutItemText}>
-                            <h3>Backend Developer</h3>
-                            <p>I have experience developing fast and optimized backend systems and APIs</p>
+                            <h3>Full Stack Developer</h3>
+                            <p>I build responsive UIs with Angular and React, using NgRx and Redux for state management.
+                                 On the back-end, I develop RESTful APIs and microservices with Node.js and Django, 
+                                 optimizing performance with Redis caching.I deploy apps using AWS services and 
+                                 automate CI/CD with Jenkins and GitLab CI. Docker and Kubernetes help with 
+                                 containerization and scaling. For security, I implement JWT, OAuth2, and encryption, 
+                                 while monitoring performance with New Relic and AWS CloudWatch.
+                            </p>
                         </div>
                     </li>
                     <li className={styles.aboutItem}>
                         <img src={pic3} alt="UI icon"/>
                         <div className={styles.aboutItemText}>
-                            <h3>UI Designer</h3>
-                            <p>I have designed multiple landing pages and have created systems as well</p>
+                            <h3>Goal</h3>
+                            <p>My goal is to create technology that makes a real impact—building scalable, secure,
+                                 and user-centric applications that solve real-world problems. I strive to continuously learn,
+                                  innovate, and push the boundaries of Full Stack Development and Cloud technologies. 
+                                  Beyond technical work, I am passionate about knowledge-sharing, fostering innovation, 
+                                  and developing leadership skills to drive meaningful change in the tech industry.</p>
                         </div>
                     </li>
                 
--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -13,10 +13,10 @@ export const ProjectCard = ({ project: { title, description, imageSrc, skills, d
                     return <li key={id} className={styles.skill}>{skill}</li>
                 })}
             </ul>
-            <div className={styles.links}>
+            { /*<div className={styles.links}>
                 <a href={demo} className={styles.link}>Demo</a>
                 <a href={source} className={styles.link}>Source</a>
-            </div>
+            </div> */}
         </div>            
 )
 } 
